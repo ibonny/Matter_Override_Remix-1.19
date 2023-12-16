3a66c9d2f22960cab281e283f7172e760d5a6ff4
Ian Bonnycastle <ibonny@gmail.com>
2023-12-16 11:45:59 -0500

    Initial commit.

---
+++ src/main/java/personal/ibonny/mo_remix/MORemix.java
@@ -0,0 +1,32 @@
+package personal.ibonny.mo_remix;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(MORemix.MOD_ID)
+public class MORemix {
+    public static final String MOD_ID = "mo_remix";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MORemix() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+    }
+}
